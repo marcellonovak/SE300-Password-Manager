31f8b991fae38941630ace18f8a29ac5b8696b78
Marcello Novak <marcellonovak@live.com>
2025-03-03 15:33:29 -0700

    Add project files.

---
+++ MainForm.cs
@@ -0,0 +1,25 @@
+namespace SE300_PasswordManager_Mockup
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PasswordViewer_DataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
